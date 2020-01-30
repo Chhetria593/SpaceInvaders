8550fc2f1cea64dbbb8106de3136390153530d8a
Chhetria593 <60403405+Chhetria593@users.noreply.github.com>
2020-01-30 13:29:29 -0500

    Update Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -2,21 +2,18 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public class Enemy {
+
 	
-	int enemyX = 0;
-	int enemyY = 0;
-
-	Enemy(int a,int b)
-		{
-		enemyX=a;
-		enemyY=b;
-		}
-
-		public void draw(Graphics g)
-		{
-		g.setColor(Color.white);
-		g.fillRect(enemyX, enemyY, 25, 25);
-		}
+		int enemyX=300;  
+		int enemyY=50;
 		
-	}
+		
+		public void draw(Graphics g)
+			{
+			g.setColor(Color.white);
+			g.fillRect(enemyX, enemyY, 25, 25);
+			}
+
+}
+
 
