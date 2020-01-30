fe1b3807d14a1e19701f16de5ce39148c9503e33
ADeveloderp <60480985+ADeveloderp@users.noreply.github.com>
2020-01-30 13:25:09 -0500

    Add files via upload

---
+++ Score.java
@@ -0,0 +1,22 @@
+package spaceInvaders;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Score {
+	int Score;
+
+public void start()
+{
+	Score = 0;
+}
+
+public void draw(Graphics g)
+{
+	//Score HUD
+	g.setColor(Color.pink);
+	g.fillRect(475, 40, 105, 20);
+	g.setColor(Color.black);
+	g.drawString("Score: " + Score, 480, 55);
+}
+}
