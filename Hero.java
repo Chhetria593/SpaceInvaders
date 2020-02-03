7cf6b148d69b51ba665b40365ff9ddbe1b0e5503
Chhetria593 <60403405+Chhetria593@users.noreply.github.com>
2020-02-03 12:17:48 -0500

    Update Hero.java

--- Hero.java
+++ Hero.java
@@ -1,32 +1,25 @@
-	import java.awt.Color;
-	import java.awt.Graphics;
 
-	public class Hero 
-	{	
-		int x=300;
-		int y=400;
-		public void draw (Graphics g)
-		{
-		g.setColor(Color.BLACK);
-		g.fillRect(x,y,40,40);
-		}
-		public void moveL()
-		{
-			x=x-20;
-			if (x==-1||x<5)
-			{
-				x=10;
-			}
-		}
-		public void moveR()
-		{
-			x=x+20;
-			if (x==601||x>550)
-			{
-				x=550;
-			}
-		}
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Hero 
+{	
+	int x=300;
+	int y=525;
+	public void draw (Graphics g)
 	{
-		
+	g.setColor(Color.BLACK);
+	g.fillRect(x,y,40,40);
 	}
+	public void moveL()
+	{
+		x=x-20;
 	}
+	public void move()
+	{
+		x=x+20;
+	}
+{
+	
+}
+}
